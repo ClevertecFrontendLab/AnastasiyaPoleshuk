c5b9c6d010a6e44692183d3fc98cee570ff88c92
AnastasiyaPoleshuk <anastasiyapaliashuk@gmail.com>
2023-02-21 14:15:24 +0300

    sprint4: add categories pages

---
+++ src/pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,23 @@
+import { useSelector } from 'react-redux';
+
+import { CardsWraper } from '../../components/CardsWraper/CardsWraper';
+import { Navigation } from '../../components/Navigation/Navigation';
+import { IStore } from '../../types/storeTypes';
+
+export const DesignPage = () => {
+    const books = useSelector((state: IStore) => state.books.books);
+    const error = useSelector((state: IStore) => state.error.error);
+
+    return (
+        <section className="content-wrap">
+            {
+                error.error.message ? null : <Navigation />
+            }
+            {
+                books.length > 0 ? <CardsWraper category='Дизайн' /> : null
+            }
+        </section>
+    )
+}
+
+
---
+++ src/pages/ParentsPage/ParentsPage.tsx
@@ -0,0 +1,21 @@
+import { useSelector } from 'react-redux';
+
+import { CardsWraper } from '../../components/CardsWraper/CardsWraper';
+import { Navigation } from '../../components/Navigation/Navigation';
+import { IStore } from '../../types/storeTypes';
+
+export const ParentsPage = () => {
+    const books = useSelector((state: IStore) => state.books.books);
+    const error = useSelector((state: IStore) => state.error.error);
+
+    return (
+        <section className="content-wrap">
+            {
+                error.error.message ? null : <Navigation />
+            }
+            {
+                books.length > 0 ? <CardsWraper category='Родителям' /> : null
+            }
+        </section>
+    )
+}
