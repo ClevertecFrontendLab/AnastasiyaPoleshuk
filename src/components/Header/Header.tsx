a148eccd21f547c8306d4546b8a16a205f745dfa
AnastasiyaPoleshuk <anastasiyapaliashuk@gmail.com>
2023-03-03 15:00:55 +0300

    sprint5: implement logout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,21 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { AppContext } from '../../context/AppContext';
+import { CONSTANTS } from '../../utils/constants';
+import { HeaderNav } from '../HeaderNav/HeaderNav';
 
 import './Header.scss';
 
 export const Header = () => {
-    const { isModalOpen, closeModal, openModal } = useContext(AppContext);
-    const [isMenuOpen, setIsMenuOpen] = useState(isModalOpen);
+    const { isBurgerModalOpen, isNavModalOpen, closeModal, openModal } = useContext(AppContext);
 
-    useEffect(() => {
-        setIsMenuOpen(isModalOpen);
-    }, [isModalOpen])
-
-    const toggleMenu = () => {
-
-        if (!isMenuOpen) {
-            openModal()
+    const toggleMenu = (type: string, modal: boolean) => {
+        if (!modal) {
+            openModal(type)
         } else {
-            closeModal()
+            closeModal(type)
         }
-        setIsMenuOpen(!isMenuOpen);
     }
 
 
@@ -28,13 +23,26 @@ export const Header = () => {
         <header className="header">
             <div className="header__container">
                 <NavLink to="/" className="header__link" />
-                <button type='button' className={isMenuOpen ? 'header__burger-active' : 'header__burger'} onClick={toggleMenu} data-test-id='button-burger' />
+                <button
+                    type='button'
+                    className={isBurgerModalOpen ? 'header__burger-active' : 'header__burger'}
+                    onClick={() => toggleMenu(CONSTANTS.BURGER_MODAL, isBurgerModalOpen)}
+                    data-test-id='button-burger'
+                />
                 <h1 className="header__title">Библиотека</h1>
                 <div className="header__profile-block profile-block">
                     <h5 className="profile__greetings">Привет, Иван!</h5>
-                    <NavLink to="/profile" className="profile__link" />
+                    <button
+                        type='button'
+                        className="profile__link"
+                        onClick={() => toggleMenu(CONSTANTS.NAV_MODAL, isNavModalOpen)}
+                    />
                 </div>
+                {
+                    isNavModalOpen && <HeaderNav styleType='header-nav__block' />
+                }
             </div>
+
         </header>
     )
 }
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import { Outlet, useNavigate } from 'react-router-dom'
 
+import { IStore } from '../../types/storeTypes';
 import { Footer } from '../Footer/Footer'
 import { Header } from '../Header/Header'
 import { MenuModal } from '../MenuModal/MenuModal';
 
-export const Layout = () => (
-    <React.Fragment>
-        <Header />
-        <div className="app">
-            <Outlet />
-        </div>
-        <Footer />
-        <MenuModal />
-    </React.Fragment>
-)
+export const Layout = () => {
+    const isAuth = useSelector((state: IStore) => state.isAuth.isAuth);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        isAuth  ? null : navigate('/auth');
+    }, [isAuth])
+
+    return (
+        <React.Fragment>
+            <Header />
+            <div className="app">
+                <Outlet />
+            </div>
+            <Footer />
+            <MenuModal />
+        </React.Fragment>
+    )
+}
