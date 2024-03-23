e52ce6a3379ad3aa8268158b286b67be42f994fa
Anastasiya Poleshuk <anastasiyapaliashuk@gmail.com>
2024-03-23 22:10:28 +0300

    feat: update button  logo

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -64,6 +64,9 @@ export const Navigation: React.FC = () => {
             case CONSTANTS.SIDEBAR_KEYS.CALENDAR:
                 dispatch(GetTrainingInfoThunk(accessToken));
                 break;
+            case CONSTANTS.SIDEBAR_KEYS.PROFILE:
+                dispatch(push(`${CONSTANTS.ROUTER__PATH.PROFILE__PATH}`));
+                break;
 
             default:
                 break;
@@ -80,7 +83,25 @@ export const Navigation: React.FC = () => {
     return (
         <div className={`nav__menu ${collapsed ? 'collapsed__menu' : 'not-collapsed__menu'}`}>
             <div className='nav__menu-section'>
-                {collapsed ? <LogoShortIcon /> : <LogoIcon />}
+                {collapsed ? (
+                    <Button
+                        className='nav__menu-section_btn'
+                        onClick={() => {
+                            dispatch(push(CONSTANTS.ROUTER__PATH.AUTH__PATH));
+                        }}
+                    >
+                        <LogoShortIcon />
+                    </Button>
+                ) : (
+                    <Button
+                        className='nav__menu-section_btn'
+                        onClick={() => {
+                            dispatch(push(CONSTANTS.ROUTER__PATH.MAIN__PATH));
+                        }}
+                    >
+                        <LogoIcon />
+                    </Button>
+                )}
                 <Button
                     type='text'
                     onClick={toggleCollapsed}
